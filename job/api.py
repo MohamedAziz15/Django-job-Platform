9d065ff8d8f4f4948f0558b2c4dcbb0867ce94a7
MohamedAziz15 <mohamedazizmahmoud@gmail.com>
2023-11-02 10:10:52 +0200

    add update Delete API

--- job/api.py
+++ job/api.py
@@ -3,7 +3,8 @@ from .models import job
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework import generics
+ 
 
 @api_view(['GET'])
 def job_list_api(request):
@@ -17,3 +18,13 @@ def job_detail_api(request,id):
     job_ = job.objects.get(id=id)
     data = jobSerializer(job_,many=False).data
     return Response({'job': data})
+
+#Class based view
+class JobListAPI(generics.ListAPIView):
+    queryset = job.objects.all()
+    serializer_class = jobSerializer
+
+
+class JobDetailAPI(generics.RetrieveAPIView,RetrieveUpdateDstroyAPIView):
+    queryset = job.objects.all()
+    serializer_class = jobSerializer
--- job/urls.py
+++ job/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from .views import job_list, job_detail
-from .api import job_list_api , job_detail_api
+from .api import job_list_api , job_detail_api, JobListAPI , JobDetailAPI
 
 
 urlpatterns = [
     path('', job_list),
     path('<slug:slug>',job_detail),
-
-
-
     
+#API as functions
+    #path('api/list',job_list_api),
+    #path('api/list/<int:id>/',job_detail_api),
 
-   path('api/list',job_list_api),
-   path('api/list/<int:id>/',job_detail_api)
+#API as class based view
+    path('api/list',JobListAPI.as_view()),
+    path('api/list/<int:id>/',JobDetailAPI.as_view()),
 
 ]
 
