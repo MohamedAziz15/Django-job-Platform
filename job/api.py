3959e5ef7b9448d08ecc7dda1327e2d66ae92ce9
MohamedAziz15 <mohamedazizmahmoud@gmail.com>
2023-11-02 06:11:11 +0200

    add job_list and detail jobs and serilaization

--- job/api.py
+++ job/api.py
@@ -11,3 +11,9 @@ def job_list_api(request):
     jobs = job.objects.all()
     data = jobSerializer(jobs, many=True).data
     return Response({'jobs': data})
+
+@api_view(['GET'])
+def job_detail_api(request,id):
+    job_ = job.objects.get(id=id)
+    data = jobSerializer(job_,many=False).data
+    return Response({'job': data})
